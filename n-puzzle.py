56ce63c2cc4952e491a63f936f414bf49a0e45d6
MBA-Aleksi <aleksi.gautier@gmail.com>
2020-05-11 18:33:15 +0200

    made function that gives the target to an n-puzzle

--- n-puzzle.py
+++ n-puzzle.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import numpy as np
 
 def gen_puzzle(n):
     puzzle = []
@@ -12,7 +13,39 @@ def gen_puzzle(n):
         numbers.pop(rand)
     return(puzzle)
 
+def cycle(sx, sy):
+    if (sx == 1 and sy == 0):
+        return (0, 1)
+    elif (sx == 0 and sy == 1):
+        return (-1, 0)
+    elif (sx == -1 and sy == 0):
+        return (0, -1)
+    else:
+        return (1, 0)
+
+def is_valid(sol, x, y):
+    n = len(sol[0])
+    if (x == n or y == n or sol[y][x] != 0):
+        return (0)
+    return (1)
+
+def gen_solution(n):
+    sol = np.zeros((n, n))
+    i = 1
+    x, y = [0, 0]
+    sx, sy = [1, 0]
+    while i != n**2:
+        if (is_valid(sol, x + sx, y + sy) == 0):
+            sx, sy = cycle(sx, sy)
+        sol[y][x] = i
+        i += 1
+        x += sx
+        y += sy
+    return sol
+
 if __name__ == "__main__":
     n = 3
-    puzzle = gen_puzzle(n)
-    print(puzzle)
+    #puzzle = gen_puzzle(n)
+    #print(puzzle)
+    solution = gen_solution(n)
+    print(solution)
