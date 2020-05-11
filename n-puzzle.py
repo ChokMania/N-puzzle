1444fbb8deb20735363d550f292f1daf7e898873
MBA-Aleksi <aleksi.gautier@gmail.com>
2020-05-11 17:51:17 +0200

    added ressources and random puzzle generator

--- n-puzzle.py
+++ n-puzzle.py
@@ -1,4 +1,18 @@
 import sys
+import random
+
+def gen_puzzle(n):
+    puzzle = []
+    numbers = []
+    for i in range (n**2):
+        numbers.append(i)
+    for i in range(n**2):
+        rand = random.randint(0, len(numbers) - 1)
+        puzzle.append(numbers[rand])
+        numbers.pop(rand)
+    return(puzzle)
 
 if __name__ == "__main__":
-    N = 3
+    n = 3
+    puzzle = gen_puzzle(n)
+    print(puzzle)
