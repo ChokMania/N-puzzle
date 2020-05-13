d2a7c7068cc4658fef4e386adc994f3568d84b98
Kelias <aleksi.gautier@gmail.com>
2020-05-13 16:16:09 +0200

    first version of solver done, needs to be optimized

--- solver.py
+++ solver.py
@@ -2,6 +2,7 @@ import numpy as np
 import generator
 import utility
 import random
+import operator
 
 def	get_listed_coords(n):
 	target = generator.gen_solution(n)
@@ -28,9 +29,11 @@ class Node:
 		self.grid = grid
 		self.h_score = h_score
 		self.dist = dist
+		self.total = h_score + dist
 
 
 def solve(grid, n):
+	solution = generator.gen_solution(n)
 	x_tar, y_tar = get_listed_coords(n)
 	h_score = manhattan_dist(grid, n, x_tar, y_tar)
 	parent = Node(None, grid, manhattan_dist(grid, n, x_tar, y_tar), 0)
@@ -40,14 +43,29 @@ def solve(grid, n):
 		open_list.append(child)
 		del child
 	closed_list = [parent]
-	print("Generated puzzle is:\n", grid, "\n")
-	for n in open_list:
-		print("\n", n.parent, n.grid, n.h_score, n.dist)
-		print("")
 	done = False
-	#while (open_list and not done):
-		
+	while (open_list and not done):
+		open_list = sorted(open_list, key=operator.attrgetter('total'))
+		parent = open_list[0]
+		for move in utility.get_moves(parent.grid):
+			accept = True
+			if (np.array_equal(move, solution)):
+				done = True
+				print("Yay!\n", move, parent.dist + 1)
+				break
+			for c in closed_list:
+				if np.array_equal(c.grid, move):
+					accept = False
+					break
+			if (accept):
+				child = Node(parent, move, manhattan_dist(move, n, x_tar, y_tar), parent.dist + 1)
+				open_list.append(child)
+				del child
+		closed_list.append(parent)
+		open_list.pop(0)
 
-n = 3
+
+n = 4
 grid = generator.gen_puzzle(n, 100)
+print(grid)
 solve(grid, n)
