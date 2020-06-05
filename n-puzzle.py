bf09b87d2a669af2f3baa658037defcd295c1ee7
Kelias <aleksi.gautier@gmail.com>
2020-06-05 14:16:06 +0200

    bug fix

--- n-puzzle.py
+++ n-puzzle.py
@@ -9,7 +9,7 @@ def manager(args):
 		else:
 			n = int(args.generate)
 			print(f"Puzzle has been created with size = {n}", end="")
-		i = int(args.iteration) if args.iteration is not None and int(args.iteration) < 1 else 100
+		i = int(args.iteration) if args.iteration is not None and int(args.iteration) > 1 else 100
 		print(f" and scrambled with {i} moves")
 		args.map = generator.gen_puzzle(n, i)
 	if check.is_solvable(args.map, args.verbose) == False:
